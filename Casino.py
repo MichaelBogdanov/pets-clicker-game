3658059ab147e9cc2c898ca38417d18d5132000c
EdZurA-M <temuk120a@gmail.com>
2026-04-26 12:52:51 +0300

    Изменены шансы выпадения

--- Casino.py
+++ Casino.py
@@ -15,12 +15,12 @@ from IronMan import IronMan
 
 
 ITEMS = {
-    Mouse: (0, (150, 150, 150)),
-    Capibara: (0, (200, 200, 255)),
-    Dog: (0, (0, 0, 255)),
-    Chinchilla: (0, (200, 100, 255)),
-    IronMan: (50, (255, 0, 0)),
-    Dino: (50, (255, 215, 0))
+    Mouse: (45, (150, 150, 150)),
+    Capibara: (20, (200, 200, 255)),
+    Dog: (15, (0, 0, 255)),
+    Chinchilla: (10, (200, 100, 255)),
+    IronMan: (7, (255, 0, 0)),
+    Dino: (3, (255, 215, 0))
 }
 
 
@@ -63,12 +63,12 @@ class Casino:
     
     def random_pet(self):
         ITEMS = {
-            Mouse: (0, (150, 150, 150)),
-            Capibara: (0, (200, 200, 255)),
-            Dog: (0, (0, 0, 255)),
-            Chinchilla: (0, (200, 100, 255)),
-            IronMan: (50, (255, 0, 0)),
-            Dino: (50, (255, 215, 0))
+            Mouse: (45, (150, 150, 150)),
+            Capibara: (20, (200, 200, 255)),
+            Dog: (15, (0, 0, 255)),
+            Chinchilla: (10, (200, 100, 255)),
+            IronMan: (7, (255, 0, 0)),
+            Dino: (3, (255, 215, 0))
         }
         chance = randint(1, 100)
         for animal, data in ITEMS.items():
